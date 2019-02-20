fe171f6e7d4d850e58bc2fad2ff3d81108af30af
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2019-02-19 22:18:17 -0700

    add in the new files w/ refactored experience

---
+++ src/components/Resume/Experience/Experience.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="experience">
+        <template v-for="experience in experiences">
+            <ExperienceCard :experience="experience" v-bind:key="experience.company+experience.position"/>
+        </template>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import ExperienceCard from './ExperienceCard.vue';
+import { Prop } from 'vue/types/options';
+import { Experience } from '@/assets/resume/resume.model';
+
+export default Vue.extend({
+    props: {
+        experiences: Array as Prop<Experience[]>,
+    },
+    components: {
+        ExperienceCard,
+    },
+});
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Resume/Experience/ExperienceCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="experience" >
+        <div class="position">{{experience.position}}</div>
+        <div class="company">{{experience.company}}</div>
+        <div class="location">
+            <div class="city-state">
+                <span class="city">{{experience.location.city}},&nbsp;</span>
+                <span class="state">{{experience.location.state}}</span>
+            </div>
+        </div>
+        <div class="time">
+            <span class="start">{{experience.startDate}}&nbsp;-&nbsp;</span>
+            <span class="end">{{experience.endDate}}</span>
+        </div>
+        <div class="accomplishments">
+            <div class="accomplishment" v-for="accomplishment in experience.accomplishments" v-bind:key="accomplishment">
+                {{accomplishment}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Prop } from 'vue/types/options';
+import { Experience } from '@/assets/resume/resume.model';
+
+export default Vue.extend({
+    props: {
+        experience: Object as Prop<Experience>,
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
