8699a524646b16925dae8cccc73890f19bdc1e4e
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2019-02-22 21:34:46 -0700

    scaffold portfolio, remove about

--- src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        THIS IS ABOUT
-    </div>
-</template>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
-        THIS IS PORTFOLIO
+    <div id="portfolio">
+        <div class="Portfolio">
+            <PortfolioCard />
+        </div>
     </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import PortfolioCard from '@/components/Portfolio/PortfolioCard.vue';
+export default Vue.extend({
+    components: {
+        PortfolioCard,
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+    #portfolio {
+
+        .portfolio {
+
+        }
+    }
+</style>
